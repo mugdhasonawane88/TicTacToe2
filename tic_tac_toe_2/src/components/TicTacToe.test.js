487fc832514f71f494a36d1f1e23079e7c0b72d4
mugdha <mugdhasonawane88@gmail.com>
2022-07-07 21:36:56 +0530

    feat: for alternate player should display O

--- tic_tac_toe_2/src/components/TicTacToe.test.js
+++ tic_tac_toe_2/src/components/TicTacToe.test.js
@@ -3,7 +3,7 @@ import TicTacToe from './TicTacToe';
 import { Constants } from '../constants/TestConstants';
 
 describe('TicTacToe component', () => {
-  
+
   beforeEach(() => {
     render(<TicTacToe />);
   })
@@ -34,7 +34,25 @@ describe('TicTacToe component', () => {
 
     squares.forEach((square, position) => {
       if (position === Constants.TOP_LEFT_SQUARE) {
-        expect(square.textContent).toBe('X');
+        expect(square.textContent).toBe(Constants.PLAYER_ONE_SYMBOL);
+      } else {
+        expect(square.textContent).toBe('');
+      }
+    })
+  });
+
+  test('Should display alternatively X & O in square when players click on a square', () => {
+
+    const squares = screen.queryAllByTestId('square');
+
+    fireEvent.click(squares[Constants.TOP_LEFT_SQUARE]);
+    fireEvent.click(squares[Constants.CENTER_SQUARE]);
+
+    squares.forEach((square, position) => {
+      if (position === Constants.TOP_LEFT_SQUARE) {
+        expect(square.textContent).toBe(Constants.PLAYER_ONE_SYMBOL);
+      } else if (position === Constants.CENTER_SQUARE) {
+        expect(square.textContent).toBe(Constants.PLAYER_TWO_SYMBOL);
       } else {
         expect(square.textContent).toBe('');
       }
--- tic_tac_toe_2/src/constants/TestConstants.js
+++ tic_tac_toe_2/src/constants/TestConstants.js
@@ -1,5 +1,8 @@
 export const Constants = {
     HEADER: 'Tic Tac Toe Game',
     TOTAL_SQUARES: 9,
-    TOP_LEFT_SQUARE: 0
+    TOP_LEFT_SQUARE: 0,
+    CENTER_SQUARE: 4,
+    PLAYER_ONE_SYMBOL: 'X',
+    PLAYER_TWO_SYMBOL: 'O',
 }
