ad27ac8b813c38fd5378a008a89ef252bd12c008
mugdha <mugdhasonawane88@gmail.com>
2022-07-07 17:35:01 +0530

    feat: display board with 3x3 squares

--- tic_tac_toe_2/src/components/TicTacToe.js
+++ tic_tac_toe_2/src/components/TicTacToe.js
@@ -2,12 +2,28 @@ import './TicTacToe.css';
 import { Constants } from '../constants/Constants';
 
 function TicTacToe() {
+
+  const renderBoard = () => {
+    return (
+      <div className='board'>
+        {
+          [...Array(Constants.TOTAL_SQUARES)].map((square, index) => {
+            return (
+              <span key={index} role='square' className='cell' />
+            )
+          }
+          )
+        }
+      </div>)
+  }
+
   return (
-    <div className="App">
-      <header data-testid="header" className="App-header">
+    <div className='App'>
+      <header data-testid='header' className='App-header'>
         {Constants.HEADER}
       </header>
-    </div>
+      {renderBoard()}
+    </div >
   );
 }
 
