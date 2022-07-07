776cd33bd645c4409d4b64c7d8b76dc3466a1f35
mugdha <mugdhasonawane88@gmail.com>
2022-07-08 00:56:13 +0530

    feat: game ends when there is a winner

--- tic_tac_toe_2/src/constants/TestConstants.js
+++ tic_tac_toe_2/src/constants/TestConstants.js
@@ -6,9 +6,14 @@ export const Constants = {
     TOP_RIGHT_SQUARE: 2,
     CENTER_LEFT_SQUARE: 3,
     CENTER_SQUARE: 4,
+    CENTER_RIGHT_SQUARE: 5,
+    BOTTOM_LEFT_SQUARE: 6,
+    BOTTOM_MIDDLE_SQUARE: 7,
+    BOTTOM_RIGHT_SQUARE: 8,
     PLAYER_ONE_SYMBOL: 'X',
     PLAYER_TWO_SYMBOL: 'O',
     PLAYER_ONE_TURN: 'Player One turn',
     PLAYER_TWO_TURN: 'Player Two turn',
-    PLAYER_ONE_WON: 'Player One won'
+    PLAYER_ONE_WON: 'Player One won',
+    PLAYER_TWO_WON: 'Player Two won'
 }
